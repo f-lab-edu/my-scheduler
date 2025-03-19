ab0beadb729d3c6288463cf4563dd4cb638ef881
DavinaWorld <kdbj12003@gmail.com>
2025-03-19 13:15:00 +0900

    tailwind v3로 다운그레이드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <h1>My Scheduler</h1>;
+export default function Page() {
+  return <div></div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
