602f82d65051a786ba5d1d136c9112be438b7d8c
DavinaWorld <kdbj12003@gmail.com>
2025-03-19 13:15:00 +0900

    tailwind v3로 다운그레이드

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>schedule page</h1>;
+  return <h1>dd</h1>;
 }
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="py-[51px] pl-[45px] text-4xl font-extrabold text-white bg-headerBlue">
+      <h1>My Scheduler</h1>
+    </div>
+  );
+}
