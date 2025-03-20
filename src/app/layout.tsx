7caa14e1cbddfd693293d591ab947a7ff2510637
DavinaWorld <kdbj12003@gmail.com>
2025-03-21 00:45:49 +0900

    button 컴파운드 패턴 사용하여  생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ type Props = Readonly<{
 export const metadata: Metadata = {
   title: "Scheduler",
   description: "스케줄을 관리해보자",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }: Props) {
