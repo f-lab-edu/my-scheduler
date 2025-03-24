b4e6c79e6f5fdb99357515a7b9226afae1c23ae9
DavinaWorld <kdbj12003@gmail.com>
2025-03-24 18:29:03 +0900

    사용 안 하는 코드 제거

--- src/components/common/button/AddColumnButton.tsx
+++ src/components/common/button/AddColumnButton.tsx
@@ -2,13 +2,8 @@ import plusIcon from "@/assets/plus.svg";
 import { useModal } from "@/hooks/useModal";
 import Image from "next/image";
 export default function AddColumnButton() {
-  const { open, openModal, closeModal } = useModal();
-
   return (
-    <button
-      className="flex items-center gap-1 p-3 hover:bg-hover-add focus:outline-none bg-button-add text-white rounded-[10px]"
-      onClick={openModal}
-    >
+    <button className="flex items-center gap-1 p-3 hover:bg-hover-add focus:outline-none bg-button-add text-white rounded-[10px]">
       <Image width="24" height="24" src={plusIcon} alt="plus icon" />
       <span>Add New Status</span>
     </button>
