5e2e55321d301c8850dd45c86befb011b3b41425
DavinaWorld <kdbj12003@gmail.com>
2025-03-24 18:20:49 +0900

    배포 정적 파일에서 전체코드 배포로 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-cd ../
-mkdir output
-cp -R ./my-scheduler/* ./output
-cp -R ./output ./my-scheduler/
+#TODO: 추후 정상 동작 확인 후 제거 예정
+# #!/bin/sh
+# cd ../
+# mkdir output
+# cp -R ./my-scheduler/* ./output
+# cp -R ./output ./my-scheduler/
