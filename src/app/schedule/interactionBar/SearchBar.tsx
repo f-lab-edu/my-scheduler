d29b8d5e46bbe1bd5946941209f5b32d6ab3ffd7
DavinaWorld <kdbj12003@gmail.com>
2025-03-25 18:57:23 +0900

    ui 수정

--- src/app/schedule/interactionBar/SearchBar.tsx
+++ src/app/schedule/interactionBar/SearchBar.tsx
@@ -2,7 +2,7 @@ import SearchIcon from "@/assets/search.svg";
 
 export default function SearchBar() {
   return (
-    <div className="flex py-4 px-[18px] gap-2 border-2 border-solid border-border-search rounded-full bg-background-searchBar">
+    <div className="flex items-center py-4 px-[18px] gap-2 border-2 border-solid border-border-search rounded-full bg-background-searchBar">
       <SearchIcon />
       <input
         className="bg-transparent outline-none text-white"
