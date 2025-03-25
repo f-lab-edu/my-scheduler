39a70a956271e2af205271b28c73166b03743078
DavinaWorld <kdbj12003@gmail.com>
2025-03-26 00:04:54 +0900

    status 내부 버튼 처리

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,22 +1,11 @@
 "use client";
 
 import Contents from "@/app/schedule/Contents";
-import Editor from "@/components/common/Editor";
-import Modal from "@/components/common/Modal";
-import { useModal } from "@/hooks/useModal";
 
 export default function Page() {
-  const { open, openModal, closeModal } = useModal();
-
   return (
     <h1>
       <Contents />
-      <button onClick={openModal}>누르기테스트</button>
-      {open && (
-        <Modal onClose={closeModal}>
-          <Editor />
-        </Modal>
-      )}
     </h1>
   );
 }
