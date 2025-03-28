39ebdd15f8960f5a3f210d63c6583915896687c6
DavinaWorld <kdbj12003@gmail.com>
2025-03-28 13:00:48 +0900

    버튼 스타일 수정

--- src/app/schedule/contents/board/Board.tsx
+++ src/app/schedule/contents/board/Board.tsx
@@ -58,7 +58,7 @@ export default function Board({
           />
         ))}
       </section>
-      <section className="flex justify-center py-5 px-3 w-[184px] ml-4 rounded-xl m-w-[150px] h-full bg-background-status">
+      <section className="flex justify-center py-5 px-3 w-[184px] ml-4 rounded-xl min-w-[250px] h-full bg-background-status">
         {isAddStatusVisible ? (
           <AddStatusInput
             onClick={handleAddStatusInputVisibility}
--- src/components/common/button/SideAddColumnButton.tsx
+++ src/components/common/button/SideAddColumnButton.tsx
@@ -7,10 +7,7 @@ interface Props {
 
 export default function SideAddColumnButton({ onClick }: Props) {
   return (
-    <button
-      className="hover:bg-headerBlue p-2 rounded-lg min-w-[184px] w-[184px]"
-      onClick={onClick}
-    >
+    <button className="hover:bg-headerBlue p-2 rounded-lg " onClick={onClick}>
       <PlusIcon width={24} height={24} />
     </button>
   );
