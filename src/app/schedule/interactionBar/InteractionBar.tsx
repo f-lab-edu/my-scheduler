ed355be43bce3a4357922e256df5521ddf419a03
DavinaWorld <kdbj12003@gmail.com>
2025-03-30 18:43:58 +0900

    total tasks 수정

--- src/app/schedule/interactionBar/InteractionBar.tsx
+++ src/app/schedule/interactionBar/InteractionBar.tsx
@@ -5,7 +5,7 @@ import FilterButton from "@/components/common/button/FilterButtons";
 import { useContentsContext } from "@/app/schedule/contents/ContentsContext";
 
 export default function InteractionBar() {
-  const { setIsAddStatusVisible } = useContentsContext();
+  const { setIsAddStatusVisible, taskList } = useContentsContext();
   const handleChangeAddStatusInput = () => {
     setIsAddStatusVisible(true);
   };
@@ -14,8 +14,7 @@ export default function InteractionBar() {
     <div className="flex items-center justify-between py-5 px-[70px]">
       <span className="flex items-center gap-3">
         <span className="flex gap-[3px] text-white text-xl">
-          {/* TODO: count 동적으로 */}
-          <span>3</span>
+          <span>{taskList.length}</span>
           <span>tasks</span>
         </span>
         <AddColumnButton onClick={handleChangeAddStatusInput} />
