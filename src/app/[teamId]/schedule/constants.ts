f5c1ad5bd4bbb0d22fa0f5a98161483e0483db45
DavinaWorld <kdbj12003@gmail.com>
2025-04-16 23:00:07 +0900

    불필요한 코드 제거

--- src/app/[teamId]/schedule/constants.ts
+++ src/app/[teamId]/schedule/constants.ts
@@ -5,20 +5,3 @@ export const confirmDeleteMessage =
 export const confirmTaskDeleteMessage = "일정을 삭제하시겠습니까?";
 export const completeDeleteMessage = "삭제 되었습니다.";
 export const confirmSaveMessage = "일정을 저장 하시겠습니까?";
-
-//month
-export const MONTH_NAMES = [
-  "JAN",
-  "FEB",
-  "MAR",
-  "APR",
-  "MAY",
-  "JUN",
-  "JUL",
-  "AGU",
-  "SEP",
-  "OCT",
-  "NOV",
-  "DEC",
-];
-export const WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
