90e0efa357222ccd3ebdf80c2fa30f9a387473a8
DavinaWorld <kdbj12003@gmail.com>
2025-04-18 01:59:30 +0900

    '/'경로 page 리팩토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import { cookies } from "next/headers";
-import { getAuth } from "firebase-admin/auth";
 import { redirect } from "next/navigation";
 import "@/lib/firebase";
+import { getSessionUid } from "@/lib/server/auth";
 import Home from "@/app/home/Home";
 export default async function Page() {
-  const cookieStore = cookies();
-  const sessionCookie = (await cookieStore).get("session")?.value;
+  const uid = await getSessionUid();
+  if (uid) redirect(`/mypage/${uid}`);
 
-  if (sessionCookie) {
-    const decodedToken = await getAuth()
-      .verifySessionCookie(sessionCookie)
-      .catch((error) => {
-        console.error("세션 검증 실패:", error);
-        return null;
-      });
-    if (decodedToken) {
-      redirect(`/mypage/${decodedToken.uid}`);
-    }
-  }
   return <Home />;
 }
