593a02ccc44ef57411084b62709e7d67226499c0
DavinaWorld <kdbj12003@gmail.com>
2025-04-18 15:20:54 +0900

    app>page 사용자 uid 체크 middleware로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { redirect } from "next/navigation";
 import "@/lib/firebase";
-import { getSessionUid } from "@/lib/server/auth";
 import Home from "@/app/home/Home";
 export default async function Page() {
-  const uid = await getSessionUid();
-  if (uid) redirect(`/mypage/${uid}`);
-
   return <Home />;
 }
