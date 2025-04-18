16b2489a60aee6fe931414496dfca6a3898807e3
DavinaWorld <kdbj12003@gmail.com>
2025-04-18 15:44:30 +0900

    미들웨어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,15 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
-    const response = await fetch(new URL("/api/auth/session", request.url));
+    const cookie = request.headers.get("cookie") ?? "";
+    const response = await fetch(new URL("/api/auth/session", request.url), {
+      headers: { cookie },
+    });
     const { uid } = await response.json();
 
     if (uid) {
-      return NextResponse.redirect(`/mypage/${uid}`);
+      const redirectUrl = new URL(`/mypage/${uid}`, request.url);
+      return NextResponse.redirect(redirectUrl);
     }
   }
 
