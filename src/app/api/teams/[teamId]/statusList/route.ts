b8d88acd885b9d182e3c5fcfe8777fe7b27a1b07
DavinaWorld <kdbj12003@gmail.com>
2025-04-21 17:18:20 +0900

    주석 제거

--- src/app/api/teams/[teamId]/statusList/route.ts
+++ src/app/api/teams/[teamId]/statusList/route.ts
@@ -1,35 +1,3 @@
-// import { NextResponse, type NextRequest } from "next/server";
-// import { getFirestore } from "firebase-admin/firestore";
-// import { StatusType } from "@/types/scheduleType";
-// import "@/lib/firebase";
-
-// export async function GET(
-//   request: NextRequest,
-//   { params }: { params: Promise<{ teamId: string }> }
-// ): Promise<NextResponse> {
-//   const { teamId } = await params;
-//   try {
-//     const snapshot = await getFirestore()
-//       .collection("teams")
-//       .doc(teamId)
-//       .collection("statusList")
-//       .get();
-
-//     const statusList = snapshot.docs.map((doc) => ({
-//       id: doc.id,
-//       ...(doc.data() as StatusType),
-//     }));
-
-//     return NextResponse.json(statusList, { status: 200 });
-//   } catch (err) {
-//     console.error(err);
-//     return NextResponse.json(
-//       { error: "서버 에러가 발생했습니다." },
-//       { status: 500 }
-//     );
-//   }
-// }
-
 import { NextResponse, type NextRequest } from "next/server";
 import { getFirestore } from "firebase-admin/firestore";
 import { StatusType } from "@/types/scheduleType";
