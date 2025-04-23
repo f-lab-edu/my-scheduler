b2e0e24703eadedeb0605527119c67d604b5017d
DavinaWorld <kdbj12003@gmail.com>
2025-04-23 15:48:48 +0900

    mypage 사용자 들어가있는 team 리스트 전시

--- src/app/mypage/[uid]/TeamScheduleList.tsx
+++ src/app/mypage/[uid]/TeamScheduleList.tsx
@@ -1,25 +1,51 @@
 "use client";
+import { useEffect, useState } from "react";
 import AddNewButton from "@/components/common/button/AddNewButton";
 import TeamScheduleCard from "./TeamScheduleCard";
 import { useModal } from "@/hooks/useModal";
 import Modal from "@/components/common/Modal";
 import InviteMember from "@/components/common/InviteMember";
+import { getMyTeams, TeamType } from "@/lib/api/teams";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 
 export default function TeamScheduleList() {
+  const [teams, setTeams] = useState<TeamType[]>([]);
   const { open, closeModal, openModal } = useModal();
+  const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getMyTeams();
+        setTeams(data);
+      } catch (error: any) {
+        // TODO: error 알럿으로 처리
+        // setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
   return (
     <div className="pt-16 px-8">
       <AddNewButton type="team" onClick={openModal} />
-      <TeamScheduleCard
-        teamName="우리팀"
-        members={[{ name: "홍길동", id: "zzyfzZi6rkVGnlXAyrfuK5KEeeH2" }]}
-      />
+      {teams.map((team, index) => (
+        <TeamScheduleCard
+          key={team.id}
+          teamName={team.teamName}
+          members={team.members}
+        />
+      ))}
 
       {open && (
         <Modal onClose={closeModal}>
           <InviteMember onClose={closeModal} />
         </Modal>
       )}
+
+      {isLoading && <LoadingSpinner />}
     </div>
   );
 }
--- src/lib/api/teams.ts
+++ src/lib/api/teams.ts
@@ -1,6 +1,11 @@
 export interface CreateTeamType {
   teamId: string;
 }
+export interface TeamType {
+  id: string;
+  teamName: string;
+  members: string[];
+}
 
 export async function createTeam(teamName: string): Promise<CreateTeamType> {
   const response = await fetch("/api/teams/create", {
@@ -13,4 +18,10 @@ export async function createTeam(teamName: string): Promise<CreateTeamType> {
   if (!response.ok) throw new Error(json.error || "팀 생성에 실패했습니다.");
 
   return json as CreateTeamType;
+}
+
+export async function getMyTeams(): Promise<TeamType[]> {
+  const response = await fetch("/api/teams/my");
+  if (!response.ok) throw new Error("팀 목록 불러오기 실패");
+  return (await response.json()) as TeamType[];
 }
