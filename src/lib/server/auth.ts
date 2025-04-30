8e98ec7dd7bc86adb4241f4304903bf21fb923c1
DavinaWorld <kdbj12003@gmail.com>
2025-04-30 15:44:50 +0900

    불필요한 코드 제거

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase-admin/auth";
 
 export async function getSessionUid(): Promise<string | null> {
   const cookieStore = await cookies();
-  console.log("🟢 raw session cookie 🟢", cookieStore.get("session")?.value);
   const session = await cookieStore.get("session")?.value;
   if (!session) return null;
   try {
     const { uid } = await getAuth().verifySessionCookie(session, true);
     return uid;
   } catch (error: any) {
-    console.error("🔴", error);
+    console.error(error);
     return null;
   }
 }
