690ab8c2f5aa7f3f0c24e55f0b24049a6b2e791c
DavinaWorld <kdbj12003@gmail.com>
2025-04-30 14:43:18 +0900

    log 추가

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,9 @@
 import * as admin from "firebase-admin";
 
+const databaseURL =
+  process.env.FIREBASE_DATABASE_URL ??
+  `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`;
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
@@ -7,7 +11,7 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    databaseURL,
   });
 }
 
