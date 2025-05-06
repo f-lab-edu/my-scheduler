ff67468ca9672bc8c8420e91d8abd4087619b2e3
DavinaWorld <kdbj12003@gmail.com>
2025-05-06 21:55:24 +0900

    useModal 커스텀 훅 테스트코드 추가

---
+++ src/hooks/useModal.test.ts
@@ -0,0 +1,31 @@
+import { renderHook, act } from "@testing-library/react";
+import { useModal } from "@/hooks/useModal";
+
+describe("useModal 훅 테스트", () => {
+  it("초기 오픈여부의 상태는 false이다", () => {
+    const { result } = renderHook(() => useModal());
+    expect(result.current.open).toBe(false);
+  });
+
+  it("openModal을 호출하면 open여부는 true가 된다", () => {
+    const { result } = renderHook(() => useModal());
+    act(() => {
+      result.current.openModal();
+    });
+    expect(result.current.open).toBe(true);
+  });
+
+  it("closeModal을 호출하면 open여부는 false가 된다", () => {
+    const { result } = renderHook(() => useModal());
+
+    act(() => {
+      result.current.openModal();
+    });
+    expect(result.current.open).toBe(true);
+
+    act(() => {
+      result.current.closeModal();
+    });
+    expect(result.current.open).toBe(false);
+  });
+});
