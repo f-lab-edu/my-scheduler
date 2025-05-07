8359fcd1602fb354528e0105c186c4e96eaa2caa
DavinaWorld <kdbj12003@gmail.com>
2025-05-07 13:00:30 +0900

    modal 중복코드 제거

--- src/hooks/useModal.test.ts
+++ src/hooks/useModal.test.ts
@@ -2,30 +2,30 @@ import { renderHook, act } from "@testing-library/react";
 import { useModal } from "@/hooks/useModal";
 
 describe("useModal 훅 테스트", () => {
+  let result: { current: any };
+  beforeEach(() => {
+    const hook = renderHook(() => useModal());
+    result = hook.result;
+  });
+
   it("초기 오픈여부의 상태는 false이다", () => {
-    const { result } = renderHook(() => useModal());
+    // const { result } = renderHook(() => useModal());
     expect(result.current.open).toBe(false);
   });
 
   it("openModal을 호출하면 open여부는 true가 된다", () => {
-    const { result } = renderHook(() => useModal());
-    act(() => {
-      result.current.openModal();
-    });
+    // const { result } = renderHook(() => useModal());
+    act(() => result.current.openModal());
     expect(result.current.open).toBe(true);
   });
 
   it("closeModal을 호출하면 open여부는 false가 된다", () => {
-    const { result } = renderHook(() => useModal());
+    // const { result } = renderHook(() => useModal());
 
-    act(() => {
-      result.current.openModal();
-    });
+    act(() => result.current.openModal());
     expect(result.current.open).toBe(true);
 
-    act(() => {
-      result.current.closeModal();
-    });
+    act(() => result.current.closeModal());
     expect(result.current.open).toBe(false);
   });
 });
